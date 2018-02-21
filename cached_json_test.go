3e984f71f20642ca755ebc5ec90f2e5fc42271b5
Alex Montoya <NeuroPyrox@Gmail.com>
2018-02-20 17:43:41 -0700

    Rename things

--- cached_json_test.go
+++ cached_json_test.go
@@ -48,13 +48,13 @@ func TestUnmarshal_ShouldUndoMarshal(t *testing.T) {
 }
 
 func TestMarshal_WhenDataIsChanged_ShouldStillReturnSameResult(t *testing.T) {
-	obj := &testCacher{A: 1, B: true}
-	data, err := Marshal(obj)
+	cacher := &testCacher{A: 1, B: true}
+	data, err := Marshal(cacher)
 	if err != nil {
 		t.Error(err)
 	}
-	obj.A = 332
-	data_copy, err := Marshal(obj)
+	cacher.A = 332
+	data_copy, err := Marshal(cacher)
 	if err != nil {
 		t.Error(err)
 	}
@@ -66,20 +66,20 @@ func TestMarshal_WhenDataIsChanged_ShouldStillReturnSameResult(t *testing.T) {
 }
 
 func TestMarshal_WhenUpdateIsCalled_ShouldReturnUpdatedResult(t *testing.T) {
-	obj_copy := &testCacher{A: 1, B: true}
-	_, err := Marshal(obj_copy)
+	cacher := &testCacher{A: 1, B: true}
+	_, err := Marshal(cacher)
 	if err != nil {
 		t.Error(err)
 	}
-	obj := &testCacher{A: 9, B: false}
-	obj_copy.A = obj.A
-	obj_copy.B = obj.B
-	Update(obj_copy)
-	data, err := Marshal(obj)
+	cacher_copy := &testCacher{A: 9, B: false}
+	cacher.A = cacher_copy.A
+	cacher.B = cacher_copy.B
+	Update(cacher)
+	data, err := Marshal(cacher)
 	if err != nil {
 		t.Error(err)
 	}
-	data_copy, err := Marshal(obj_copy)
+	data_copy, err := Marshal(cacher_copy)
 	if err != nil {
 		t.Error(err)
 	}
@@ -90,22 +90,22 @@ func TestMarshal_WhenUpdateIsCalled_ShouldReturnUpdatedResult(t *testing.T) {
 	}
 }
 
-func TestMarshal_WhenItHasAlreadyBeenCalled_ShouldStillUndoUnmarshal(t *testing.T) {
-	obj1 := &testCacher{A: 1, B: true}
-	obj2 := &testCacher{A: 4, B: false}
-	_, err := Marshal(obj1)
+func TestMarshal_AfterUnmarshalIsCalled_ShouldReturnUpdatedResult(t *testing.T) {
+	cacher1 := &testCacher{A: 1, B: true}
+	cacher2 := &testCacher{A: 4, B: false}
+	_, err := Marshal(cacher1)
 	if err != nil {
 		t.Error(err)
 	}
-	data, err := Marshal(obj2)
+	data, err := Marshal(cacher2)
 	if err != nil {
 		t.Error(err)
 	}
-	err = Unmarshal(data, obj1)
+	err = Unmarshal(data, cacher1)
 	if err != nil {
 		t.Error(err)
 	}
-	data_copy, err := Marshal(obj1)
+	data_copy, err := Marshal(cacher1)
 	if err != nil {
 		t.Error(err)
 	}

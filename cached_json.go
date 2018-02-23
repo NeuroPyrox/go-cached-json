cccad91baac69e7d0f5f2b96b9929fc73d034150
Alex Montoya <NeuroPyrox@Gmail.com>
2018-02-22 21:13:12 -0700

    Made thread-safe with RWMutex

--- cached_json.go
+++ cached_json.go
@@ -3,28 +3,42 @@ package cachedjson
 import (
 	"encoding/json"
 	"fmt"
+	"sync"
 )
 
 type Cache struct {
-	data []byte
+	mutex sync.RWMutex
+	data  []byte
 }
 
 func (cache *Cache) Clear() {
+	cache.mutex.Lock()
 	cache.data = nil
+	cache.mutex.Unlock()
 }
 
 func (cache *Cache) IsEmpty() bool {
+	cache.mutex.RLock()
+	defer cache.mutex.RUnlock()
+	return cache.isEmpty()
+}
+
+func (cache *Cache) isEmpty() bool {
 	return cache.data == nil
 }
 
 func (cache *Cache) Marshal() ([]byte, error) {
-	if cache.IsEmpty() {
+	cache.mutex.RLock()
+	defer cache.mutex.RUnlock()
+	if cache.isEmpty() {
 		return nil, fmt.Errorf("JSON cache is empty")
 	}
 	return cache.data, nil
 }
 
 func (cache *Cache) Unmarshal(data []byte, obj interface{}) error {
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
 	err := json.Unmarshal(data, obj)
 	if err != nil {
 		return err
@@ -34,6 +48,12 @@ func (cache *Cache) Unmarshal(data []byte, obj interface{}) error {
 }
 
 func (cache *Cache) Update(obj interface{}) error {
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
+	return cache.update(obj)
+}
+
+func (cache *Cache) update(obj interface{}) error {
 	data, err := json.Marshal(obj)
 	if err != nil {
 		return err
@@ -43,8 +63,10 @@ func (cache *Cache) Update(obj interface{}) error {
 }
 
 func (cache *Cache) UpdateIfEmpty(obj interface{}) error {
-	if cache.IsEmpty() {
-		return cache.Update(obj)
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
+	if cache.isEmpty() {
+		return cache.update(obj)
 	}
 	return nil
 }
